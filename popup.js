e41b31138483329ebc2aac9211a1d8c5776670e0
jdszekeres <jdszekeres@gmail.com>
2026-06-05 12:47:28 -0700

    Added highlighting support for html snippet

--- popup.js
+++ popup.js
@@ -191,8 +191,6 @@ render();
 }
 
 function updateProductEndings(endings) {
-    const html = generateHTML(JSON.stringify(prods), JSON.stringify(productEndings));
-    document.getElementById('html_output').textContent = html;
     const productEndingsList = document.getElementById('product_endings_ul');
     productEndingsList.innerHTML = '';
     endings.forEach(ending => {
@@ -203,12 +201,21 @@ function updateProductEndings(endings) {
 
 }
 
+function updateHTML() {
+    const html = generateHTML(JSON.stringify(prods), JSON.stringify(productEndings));
+    const htmlElement = document.getElementById('html_output');
+    htmlElement.textContent = html;
+
+    hljs.highlightElement(htmlElement, { language: 'html' });
+}
+
 document.getElementById('add_ending_button').addEventListener('click', () => {
     const newEnding = document.getElementById('new_ending_input').value.trim();
     if (newEnding && !productEndings.includes(newEnding)) {
         productEndings.push(newEnding);
         updateProductEndings(productEndings);
         document.getElementById('new_ending_input').value = '';
+        updateHTML(); 
     }
 });
 
@@ -226,8 +233,7 @@ function start() {
         
         document.getElementById('product_count').textContent = products.length - 5;
 
-        const html = generateHTML(JSON.stringify(prods), JSON.stringify(productEndings));
-        document.getElementById('html_output').textContent = html;
+        updateHTML();
     });
 }
 
